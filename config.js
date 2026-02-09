cdc95376d3262c4eb9661803b3347278829acf15
creo-create <newrob662@gmail.com>
2026-02-09 11:09:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -42,8 +42,8 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
+        extreme: "WOOOOW You love me that much?? 🥺💗",  // Shows when they go past 5000%
+        high: "To infinity and beyond! 🚀💖",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
 
@@ -51,7 +51,7 @@ const CONFIG = {
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 😣💗",
         message: "Now come get your gift, a big warm hug and a huge kiss! and Many More on the 14th!",
-        emojis: "🎁💖💝💋❤️💕"  // These will bounce around
+        emojis: "🎁💗🤗💋💐"  // These will bounce around
     },
 
     // Color scheme for the website
